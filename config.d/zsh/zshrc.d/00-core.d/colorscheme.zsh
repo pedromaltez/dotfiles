4a8b315eefa0f29bc28aec37e9208608e292fc4d
Pedro Maltez <maltezpedro@gmail.com>
2015-01-21 21:31:50 +0000

    Change terminal colorscheme location

--- config.d/zsh/zshrc.d/00-core.d/colorscheme.zsh
+++ config.d/zsh/zshrc.d/00-core.d/colorscheme.zsh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
 
 BASE16_SCHEME=ashes.dark
-BASE16_SHELL=$XDG_CONFIG_HOME/base16/base16-shell/base16-$BASE16_SCHEME.sh
+BASE16_SHELL=$XDG_CACHE_HOME/base16/base16-shell/base16-$BASE16_SCHEME.sh
 [[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
