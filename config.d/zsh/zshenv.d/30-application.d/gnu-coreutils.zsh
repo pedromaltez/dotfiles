dad4e078eeb94db4f341ca8ce0da1110bb89f6f6
Pedro Maltez <maltezpedro@gmail.com>
2015-01-21 21:52:32 +0000

    Move OS X gnu-coreutils path to zshrc.d

--- config.d/zsh/zshenv.d/30-application.d/gnu-coreutils.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-if [[ $PLATFORM == "darwin" ]]; then
-  # Use gnu Coreutils instead of Apple's
-  export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-  export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
-fi
--- config.d/zsh/zshrc.d/00-core.d/gnu-coreutils.zsh
+++ config.d/zsh/zshrc.d/00-core.d/gnu-coreutils.zsh
@@ -1,5 +1,12 @@
-#!/usr/bin/zsh
+#!/usr/bin/env zsh
+
+if [[ $PLATFORM == "darwin" ]]; then
+  # Use gnu Coreutils instead of Apple's
+  export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+  export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+fi
+
 alias la="ls -la"
 # Kind of like 'la', but with permissions displayed in octal. Doesn't work
 # great.
-alias lao="stat --printf='%A\t%a\t%h\t%U\t%G\t%s\t%.19y\t%n\n' *(DN) | numfmt --to=iec-i --field=6 --delimiter='	' --suffix=B"
+alias lao="stat --printf='%A\t%a\t%h\t%U\t%G\t%s\t%.19y\t%n\n' *(DN) | numfmt --to=iec-i --field=6 --delimiter='  ' --suffix=B"
