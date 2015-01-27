84140cb5efe52f561b6a9cc7063af8332712a25c
Pedro Maltez <maltezpedro@gmail.com>
2015-01-27 05:55:55 +0000

    zsh: Deduplicate exporting of XDG_CONFIG_HOME

--- config.d/zsh/zshenv.d/00-core.d/10-xdg.zsh
+++ config.d/zsh/zshenv.d/00-core.d/10-xdg.zsh
@@ -1,13 +1,10 @@
 #!/usr/bin/env zsh
 
-# Duplicate what is present in /etc/zsh/zshenv
-# Set the remaining XDG base directories
+# Set the XDG base directories
 [[ -n $XDG_CONFIG_HOME ]] || export XDG_CONFIG_HOME=$HOME/.config
 [[ -n $XDG_CACHE_HOME  ]] || export  XDG_CACHE_HOME=$HOME/.cache
 [[ -n $XDG_DATA_HOME   ]] || export   XDG_DATA_HOME=$HOME/.local/share
-# [[ -n $XDG_RUNTIME_DIR ]] || export XDG_RUNTIME_DIR=$HOME/.runtime
-# actual: /run/user/1000
 
 # Extra directory used for personal binaries
-# Not under the XDG namespace because it is _not_ part of the XDG spec
+# Not under the XDG namespace because it is __not__ part of the XDG spec
 export USER_BIN_HOME=$HOME/usr
